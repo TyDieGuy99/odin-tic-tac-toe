9f7ee3b0da0faa41f24969ff782a7797298a79b2
TyDieGuy99 <120081348+TyDieGuy99@users.noreply.github.com>
2025-08-29 13:53:18 -0400

    created gameboard and player objects players get names and marks, gameboard creates 2d array and has get/set

--- javascript.js
+++ javascript.js
@@ -1,1 +1,34 @@
+const gameBoard = (function () {
+    let board = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ];
+    const getSquare = (index1, index2) => {
+        return board[index1][index2];
+    };
+
+    const setSquare = (index1, index2) => {
+        board[index1][index2] = mark;
+    };
+
+    return {board, getSquare, setSquare};
+})();
+
+function createPlayer (name, mark) {
+    const playerName = name;
+    const playerMark = mark;
+    return {playerName, playerMark};
+};
+
+function gameState () {
+
+};
+
+const player1 = createPlayer('tom', 'X');
+const player2 = createPlayer('bill', 'O');
+
 console.log('everything works!');
+console.log(player1);
+console.log(player2);
+
