6f7d1d2874725ee757c74226c87f50d099c9a689
TyDieGuy99 <120081348+TyDieGuy99@users.noreply.github.com>
2025-09-09 15:43:00 -0400

    added function to gameController check for winCon - still in work

--- javascript.js
+++ javascript.js
@@ -85,6 +85,25 @@ function gameController(
         printNewTurn();
     };
 
+    const winningCondition = () => {
+        const winningBoard = 
+        [[0, 1 ,2],
+         [3, 4, 5],
+         [6, 7 ,8],
+         [0, 3, 6],
+         [1, 4, 7],
+         [2, 5, 8],
+         [0, 4, 8],
+         [2, 4, 6]];
+
+         const {a, b, c} = winningBoard;
+         spotA = a.getValue();
+         spotB = b.getValue();
+         spotC = c.getValue();
+    }
+
+   
+
     printNewTurn();
     return {playTurn};
     
